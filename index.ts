657d4f62348e0670ecccec9ee6d6931b05a5a1d9
KhanhNguyenx <nmkhanh121203@gmail.com>
2025-05-13 20:42:12 +0700

    Connect Database

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello, Music App!");
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import * as database from "./config/database";
+
+dotenv.config();
+
+database.connect();
+
+const app: Express = express();
+const port: number | string  = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
