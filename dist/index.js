45f208296a5211a7a0ee50667938355d1c0a9d2a
KhanhNguyenx <nmkhanh121203@gmail.com>
2025-05-14 23:20:09 +0700

    Configure Project and Create Topic Page

---
+++ dist/index.js
@@ -0,0 +1,40 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const path = __importStar(require("path"));
+const database = __importStar(require("./config/database"));
+const index_route_1 = __importDefault(require("./routers/client/index.route"));
+dotenv_1.default.config();
+database.connect();
+const app = express_1.default();
+const port = process.env.PORT || 3000;
+app.use(express_1.default.static("public"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+index_route_1.default(app);
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import * as path from "path";
 import * as database from "./config/database";
+import clientRoutes from "./routers/client/index.route";
 
 dotenv.config();
 
@@ -8,6 +10,14 @@ database.connect();
 
 const app: Express = express();
 const port: number | string  = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+// Client Routes
+clientRoutes(app);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
